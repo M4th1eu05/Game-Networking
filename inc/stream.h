d6f83abfbe04d7897fa3f61a3b0f0cc2e1345aef
M4th1eu05 <mathieusparfel.sti2d@gmail.com>
2025-02-11 20:17:40 -0500

    add stream.h

---
+++ inc/stream.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include <span>
+#include <cstdint>
+
+class Stream {
+public:
+    Stream(uint32_t id, bool reliable);
+    ~Stream();
+
+    void SendData(std::span<const char> data);
+    void OnDataReceived(std::span<const char> data);
+
+private:
+    uint32_t streamID;
+    bool reliable;
+};
+
