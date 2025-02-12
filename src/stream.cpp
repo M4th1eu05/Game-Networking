8e827bd60c6342229fbf88bbeb9296a711880ad5
M4th1eu05 <mathieusparfel.sti2d@gmail.com>
2025-02-11 20:24:02 -0500

    add stream.cpp

---
+++ src/stream.cpp
@@ -0,0 +1,18 @@
+﻿#include "stream.h"
+
+Stream::Stream(uint32_t id, bool reliable)
+    : streamID(id), reliable(reliable)
+{
+}
+
+Stream::~Stream()
+{
+}
+
+void Stream::SendData(std::span<const char> data)
+{
+}
+
+void Stream::OnDataReceived(std::span<const char> data)
+{
+}
