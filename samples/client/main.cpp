2a1ba85882d274216c6c1a7b3bdaf55b84562b3e
Tarook <Tarook.Zinou@gmail.com>
2025-02-15 20:36:27 -0500

    Fix: Remove old code
    useless poo poo burh moment

--- samples/client/main.cpp
+++ samples/client/main.cpp
@@ -8,20 +8,8 @@ int main() {
     spdlog::set_level(spdlog::level::debug);
     spdlog::debug("This is the client!");
 
-    auto falcon = Falcon::Connect("127.0.0.1", 5556);
-    std::string message = "This message was sent by the client!";
-    std::span data(message.data(), message.size());
-    falcon->SendTo("127.0.0.1", 5555, data);
-
-    std::string from_ip;
-    from_ip.resize(255);
-    std::array<char, 65535> buffer;
-    falcon->ReceiveFrom(from_ip, buffer);
-    spdlog::debug("Received message: {}", buffer.data());
-    return EXIT_SUCCESS;
-
     Falcon client;
-    client.Connect("127.0.0.1", 5556);
+    client.ConnectTo("127.0.0.1", 5556);
 
     client.OnConnectionEvent([&](bool success, uint64_t clientID) {
         if (success) {
--- samples/server/main.cpp
+++ samples/server/main.cpp
@@ -9,24 +9,6 @@ int main()
     spdlog::set_level(spdlog::level::debug);
     spdlog::debug("This is the server!");
 
-    auto falcon = Falcon::Listen("127.0.0.1", 5555);
-    std::string from_ip;
-    from_ip.resize(255);
-    std::array<char, 65535> buffer;
-    int recv_size = falcon->ReceiveFrom(from_ip, buffer);
-    spdlog::debug("Received message: {}", buffer.data());
-
-    std::string ip = from_ip;
-    uint16_t port = 0;
-    auto pos = from_ip.find_last_of (':');
-    if (pos != std::string::npos) {
-        ip = from_ip.substr (0,pos);
-        std::string port_str = from_ip.substr (++pos);
-        port = atoi(port_str.c_str());
-    }
-    falcon->SendTo(ip, port, std::span {buffer.data(), static_cast<unsigned long>(recv_size)});
-    return EXIT_SUCCESS;
-
     Falcon server;
     server.Listen("127.0.0.1", 5555);
 
