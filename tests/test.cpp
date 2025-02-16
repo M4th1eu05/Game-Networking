332d472f8c1fe49228eb5080b1d0fe1c86cbdf8b
Tarook <Tarook.Zinou@gmail.com>
2025-02-15 21:09:59 -0500

    Feat: Add stream tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -33,3 +33,37 @@ TEST_CASE("Client can connect to server", "[Socket]") {
     REQUIRE(connectionSuccess == true);
     REQUIRE(clientID > 0);
 }
+
+TEST_CASE("Stream sends and receives data", "[Stream]") {
+    Stream stream(1, false); // Stream non fiable
+
+    std::string message = "Hello, world!";
+    std::span<const char> data(message.data(), message.size());
+
+    bool received = false;
+    stream.OnDataReceived([&](std::span<const char> receivedData) {
+        REQUIRE(std::string(receivedData.begin(), receivedData.end()) == message);
+        received = true;
+    });
+
+    stream.SendData(data);
+
+    REQUIRE(received == true);
+}
+
+TEST_CASE("Reliable Stream retransmits lost packets", "[Stream]") {
+    Stream stream(2, true);
+
+    std::string message = "Important data!";
+    std::span<const char> data(message.data(), message.size());
+
+    stream.SendData(data);
+
+    // Simule la perte de paquets
+    stream.Acknowledge(1); // On ne confirme que le premier envoi
+
+    // Attendre pour voir si le paquet est renvoyé
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+
+    REQUIRE(true); // Si on arrive ici, c'est que le test est passé
+}
